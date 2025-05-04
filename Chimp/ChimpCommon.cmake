92b05650b73701b4d725187608d2e3000c51d036
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-05-04 16:07:41 +0100

    setup chimp target cmake function

---
+++ Chimp/ChimpCommon.cmake
@@ -0,0 +1,25 @@
+function(setup_chimp_target target_name)
+    target_compile_definitions(${target_name} PUBLIC
+        CHIMP_FLECS=1
+        CHIMP_LIBSNDFILE=1
+    )
+
+    target_include_directories(${target_name} PUBLIC
+        "${CMAKE_CURRENT_SOURCE_DIR}/../ChimpFramework/src/"
+    )
+
+    find_package(flecs CONFIG REQUIRED)
+    find_package(imgui CONFIG REQUIRED)
+
+    target_link_libraries(${target_name} PRIVATE imgui flecs::flecs_static)
+
+    if (CMAKE_VERSION VERSION_GREATER 3.12)
+        set_property(TARGET ${target_name} PROPERTY CXX_STANDARD 20)
+    endif()
+
+    if (CMAKE_BUILD_TYPE STREQUAL "Release")
+        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="Data")
+    else()
+        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="${CMAKE_CURRENT_SOURCE_DIR}/Data")
+    endif()
+endfunction()
