5390d3e3ca5b1c414044149026341753367845a9
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-05-07 11:48:05 +0100

    chimp game is now a dll, i think this makes sense

--- Chimp/ChimpCommon.cmake
+++ Chimp/ChimpCommon.cmake
@@ -1,11 +1,31 @@
+function(setup_chimp_executable target_name)
+    # Define GAME_DATA_FOLDER
+    if (CMAKE_BUILD_TYPE STREQUAL "Release")
+        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="Data")
+    else()
+        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="${CMAKE_CURRENT_SOURCE_DIR}/Data")
+    endif()
+
+    # Copy data folder only in release
+    if (CMAKE_BUILD_TYPE STREQUAL "Release")
+        add_custom_command(TARGET ${target_name} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory
+                ${CMAKE_CURRENT_SOURCE_DIR}/Data ${CMAKE_BINARY_DIR}/Data
+        )
+    endif()
+endfunction()
+
 function(setup_chimp_target target_name)
-    target_include_directories(${target_name} PUBLIC "Source/") # So you don't need to put Source/ at the start of any includes
+    target_include_directories(${target_name} PUBLIC "Source/")
+    target_include_directories(${target_name} PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/../ChimpFramework/Source/")
+
     target_link_libraries(${target_name} PRIVATE Chimp)
+    link_chimp_dependencies(${target_name})
 
     add_custom_command(TARGET ${target_name} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-       $<TARGET_FILE:Chimp>
-       $<TARGET_FILE_DIR:${target_name}>
+            $<TARGET_FILE:Chimp>
+            $<TARGET_FILE_DIR:${target_name}>
     )
 
     target_compile_definitions(${target_name} PUBLIC
@@ -13,31 +33,9 @@ function(setup_chimp_target target_name)
         CHIMP_LIBSNDFILE=1
     )
 
-    target_include_directories(${target_name} PUBLIC
-        "${CMAKE_CURRENT_SOURCE_DIR}/../ChimpFramework/Source/"
-    )
-
-    link_chimp_dependencies(${target_name})
-
     if (CMAKE_VERSION VERSION_GREATER 3.12)
         set_property(TARGET ${target_name} PROPERTY CXX_STANDARD 20)
     endif()
-
-    if (CMAKE_BUILD_TYPE STREQUAL "Release")
-        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="Data")
-    else()
-        target_compile_definitions(${target_name} PUBLIC GAME_DATA_FOLDER="${CMAKE_CURRENT_SOURCE_DIR}/Data")
-    endif()
-
-    # Copy data folder in release
-    if (CMAKE_BUILD_TYPE STREQUAL "Release")
-        file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/Data)
-        add_custom_command(TARGET ${target_name} POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${CMAKE_CURRENT_SOURCE_DIR}/Data ${CMAKE_BINARY_DIR}/Data
-    )
-endif()
-
 endfunction()
 
 function(link_chimp_dependencies target_name)
---
+++ Chimp/ChimpGame/Source/TestClass.cpp
@@ -0,0 +1,8 @@
+#include "TestClass.h"
+#include <iostream>
+
+namespace ChimpGame {
+	void TestClass::test() {
+		std::cout << "hi\n";
+	}
+}
